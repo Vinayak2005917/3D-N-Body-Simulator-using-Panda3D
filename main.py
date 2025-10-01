1e895b30586d05bf33171e90ffe0947d0d1ea4af
Vinayak <145529911+Vinayak2005917@users.noreply.github.com>
2025-10-01 10:32:59 +0530

    removed the sky box, added a proper camera movement setup, added a readme and req file

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from panda3d.core import LineSegs, NodePath
 from direct.gui.OnscreenImage import OnscreenImage
 from panda3d.core import TransparencyAttrib
 from panda3d.core import CardMaker, TextureStage, Loader
+from panda3d.core import Vec3
 import Object
 import numpy as np
 
@@ -16,35 +17,46 @@ class Main(ShowBase):
         props.setSize(1280, 720)   # width x height
         props.setTitle("3D N Body Sim")
         self.win.requestProperties(props)
+        self.is_dragging = False
+        self.last_mouse_pos = None
+
+        # Listen for mouse button events
+        self.accept("mouse1", self.startDrag)   # left mouse down
+        self.accept("mouse1-up", self.endDrag)  # left mouse up
+
+        self.taskMgr.add(self.dragCamera, "dragCamera")
 
         self.setupCamera()
 
-        self.setupSkybox()
 
         # First earth
         self.earth = Object.Object3D(
             self, "models/earth.glb",
             position=(0, 0, 0),
             scale=1,
-            rotation=(0, 180, 0)
+            rotation=(0, 90, 0)
         )
 
         # Second earth (will move)
-        self.earth2 = Object.Object3D(
+        self.moon = Object.Object3D(
             self, "models/moon.glb",
             position=(250, 0, 0),
             scale=30,
-            rotation=(0, 180, 0),
-            velocity=(0, 0, 500),
+            rotation=(0, 90, 0),
+            velocity=(0, 500, 0),
             mass=0.125
         )
         grid = self.make_grid(size=100000, step=100, z=0)
         grid.reparentTo(self.render)
-        grid.setHpr(0, 90, 0)
+        grid.setHpr(0, 0, 0)
 
         # Add update task
-        self.taskMgr.add(self.apply_gravity_on, "updateTask", extraArgs=[self.earth2], appendTask=True)
+        self.taskMgr.add(self.apply_gravity_on, "updateTask", extraArgs=[self.moon], appendTask=True)
+        self.taskMgr.add(self.updateCamera, "updateCamera")
     
+
+#--------------------------Physics----------------------------------
+
     def acceleration(self, pos_obj, pos_target, G, m_target):
         r_vec = np.array(pos_target) - np.array(pos_obj)
         r2 = np.dot(r_vec, r_vec)
@@ -97,6 +109,7 @@ class Main(ShowBase):
 
         return task.cont
     
+#--------------------------Grid----------------------------------
         
     def make_grid(self, size=1000, step=100, z=0):
         ls = LineSegs()
@@ -114,49 +127,73 @@ class Main(ShowBase):
         node = ls.create()
         return NodePath(node)
     
+#--------------------------Camera----------------------------------
+
     def setupCamera(self):
         self.disableMouse()
-        self.camera.setPos(0, -400, -800)
+        self.camera.setPos(0, 0, 0)
         self.camera.lookAt(0, 0, 0)
-        crosshair = OnscreenImage(image='crosshairs.png', pos=(0, 0, 0), scale=0.04)
-        crosshair.setTransparency(TransparencyAttrib.MAlpha)
 
-    def setupSkybox(self):
-        loader = self.loader
-        size = 5000
+    def updateCamera(self, task):
+        dt = task.dt
+        speed = 350000   # units per second
 
-        # Six faces of the skybox
-        faces = {
-            'up': 'kurt/space_up.png',
-            'down': 'kurt/space_dn.png',
-            'left': 'kurt/space_lf.png',
-            'right': 'kurt/space_rt.png',
-            'front': 'kurt/space_ft.png',
-            'back': 'kurt/space_bk.png'
-        }
+        # --- Movement vector ---
+        move_vec = Vec3(0, 0, 0)
 
-        # Positions and rotations for each face
-        transforms = {
-            'up': ((0, 0, size), (0, 180, 0)),
-            'down': ((0, 0, -size), (0, 0, 0)),
-            'left': ((-size, 0, 0), (0, 90, 0)),
-            'right': ((size, 0, 0), (0, -90, 0)),
-            'front': ((0, size, 0), (90, 0, 0)),
-            'back': ((0, -size, 0), (-90, 0, 0))
-        }
+        # WASD relative to where the camera is facing
+        if self.mouseWatcherNode.is_button_down('w'):
+            move_vec += Vec3(0, 1, 0)   # forward
+        if self.mouseWatcherNode.is_button_down('s'):
+            move_vec += Vec3(0, -1, 0)  # backward
+        if self.mouseWatcherNode.is_button_down('a'):
+            move_vec += Vec3(-1, 0, 0)  # left
+        if self.mouseWatcherNode.is_button_down('d'):
+            move_vec += Vec3(1, 0, 0)   # right
 
-        for face, tex_path in faces.items():
-            cm = CardMaker(face)
-            cm.setFrame(-1, 1, -1, 1)
-            card = self.render.attachNewNode(cm.generate())
-            card.setPos(*transforms[face][0])
-            card.setHpr(*transforms[face][1])
-            tex = loader.loadTexture(tex_path)
-            card.setTexture(tex)
-            card.setScale(size)
-            card.setBin('background', 1)
-            card.setDepthWrite(False)
-            card.setTwoSided(True)
+        # Space/Shift = up/down in WORLD space (not camera space)
+        if self.mouseWatcherNode.is_button_down('space'):
+            self.camera.setZ(self.camera.getZ() + speed * dt)
+        if self.mouseWatcherNode.is_button_down('shift'):
+            self.camera.setZ(self.camera.getZ() - speed * dt)
+
+        # --- Apply WASD in camera's local XY plane ---
+        if move_vec.length() > 0:
+            move_vec.normalize()
+            self.camera.setPos(self.camera, move_vec * speed * dt)
+
+        return task.cont
+
+    def startDrag(self):
+        self.is_dragging = True
+        if self.mouseWatcherNode.hasMouse():
+            mpos = self.mouseWatcherNode.getMouse()
+            self.last_mouse_pos = (mpos.getX(), mpos.getY())
+
+    def endDrag(self):
+        self.is_dragging = False
+        self.last_mouse_pos = None
+
+    def dragCamera(self, task):
+        if self.is_dragging and self.mouseWatcherNode.hasMouse():
+            mpos = self.mouseWatcherNode.getMouse()
+            x, y = mpos.getX(), mpos.getY()
+
+            if self.last_mouse_pos:
+                dx = x - self.last_mouse_pos[0]
+                dy = y - self.last_mouse_pos[1]
+
+                # Adjust sensitivity
+                sensitivity = 100
+
+                # Rotate camera
+                self.camera.setH(self.camera.getH() - dx * sensitivity)
+                self.camera.setP(self.camera.getP() + dy * sensitivity)
+
+            self.last_mouse_pos = (x, y)
+
+        return task.cont
+
 
 main = Main()
 main.run()
